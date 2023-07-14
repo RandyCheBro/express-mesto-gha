6745821af3dda8790ad6fcd904e953fbc3839df5
RandyCheBro <kolyavolya@yandex.ru>
2023-07-14 05:14:10 +0300

    work 14 env

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,2 @@
-require('dotenv').config();
-
-const { JWT_SECRET = 'some-secret-key' } = process.env;
 module.exports.linkRegex = /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.(ru|com)))(:\d{2,5})?((\/.+)+)?\/?#?/;
-module.exports.jwtSecret = JWT_SECRET;
+module.exports.jwtSecret = 'some-secret-key';
